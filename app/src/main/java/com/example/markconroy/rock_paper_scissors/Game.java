11d0cbf4c4765dcd8c731160aa0a8d4236065a30
DeepCFoxXx <learncode23@gmail.com>
2018-06-18 13:40:19 +0100

    started game class

--- app/src/main/java/com/example/markconroy/rock_paper_scissors/Game.java
+++ app/src/main/java/com/example/markconroy/rock_paper_scissors/Game.java
@@ -3,4 +3,12 @@ package com.example.markconroy.rock_paper_scissors;
 import java.util.Random;
 
 public class Game {
+    
+    public Selection getComputerSelection() {
+        Selection[] options = Selection.values();
+        Random rand = new Random();
+        int index = rand.nextInt(options.length);
+        return options[index];
+    }
+
 }
