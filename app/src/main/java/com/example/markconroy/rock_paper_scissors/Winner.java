320ba9826c1347e0379141d06b3656aaf4981565
DeepCFoxXx <learncode23@gmail.com>
2018-06-18 13:29:21 +0100

    winner class added

--- app/src/main/java/com/example/markconroy/rock_paper_scissors/Winner.java
+++ app/src/main/java/com/example/markconroy/rock_paper_scissors/Winner.java
@@ -1,4 +1,15 @@
 package com.example.markconroy.rock_paper_scissors;
 
 public enum Winner {
+
+    USER("Player Wins"),
+    ANDROID("Android wins"),
+    DRAW("Draw!");
+
+    private String value;
+
+    private Winner(String value) {
+        this.value = value;
+    }
+
 }
