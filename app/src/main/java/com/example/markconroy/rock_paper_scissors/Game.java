aa4144c925cacf853340a5a08a75eb224b1a1675
DeepCFoxXx <learncode23@gmail.com>
2018-06-18 13:55:09 +0100

    game logic added

--- app/src/main/java/com/example/markconroy/rock_paper_scissors/Game.java
+++ app/src/main/java/com/example/markconroy/rock_paper_scissors/Game.java
@@ -3,12 +3,29 @@ package com.example.markconroy.rock_paper_scissors;
 import java.util.Random;
 
 public class Game {
-    
+
     public Selection getComputerSelection() {
         Selection[] options = Selection.values();
         Random rand = new Random();
         int index = rand.nextInt(options.length);
         return options[index];
+    }
+
+    public Winner getWinner(Selection userChoice, Selection androidChoice) {
+
+        if (userChoice == androidChoice) {
+            return Winner.DRAW;
+        }
+
+        switch (userChoice) {
+            case ROCK:
+                return (androidChoice == Selection.SCISSORS) ? Winner.USER : Winner.ANDROID;
+            case PAPER:
+                return (androidChoice == Selection.ROCK) ? Winner.USER : Winner.ANDROID;
+            case SCISSORS:
+                return (androidChoice == Selection.PAPER) ? Winner.USER : Winner.ANDROID;
+        }
+        return Winner.DRAW;
     }
 
 }
