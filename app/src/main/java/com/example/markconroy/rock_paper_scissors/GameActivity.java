9b5321c55e19efe63eb34fe73fec193ba2083e00
DeepCFoxXx <learncode23@gmail.com>
2018-06-26 11:53:37 +0100

    started game activity

--- app/src/main/java/com/example/markconroy/rock_paper_scissors/GameActivity.java
+++ app/src/main/java/com/example/markconroy/rock_paper_scissors/GameActivity.java
@@ -2,12 +2,22 @@ package com.example.markconroy.rock_paper_scissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_game);
-//    }
+    Game game;
+    TextView computerChoiceText;
+    TextView userChoiceText;
+    TextView resultText;
+    Button buttonRock;
+    Button buttonPaper;
+    Button buttonScissors;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        game = new Game();
+    }
+
 }
