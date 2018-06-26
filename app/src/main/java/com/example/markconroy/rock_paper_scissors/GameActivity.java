a74987a8bd9697ef4326765b7af6166c09cc1d4b
DeepCFoxXx <learncode23@gmail.com>
2018-06-26 12:00:51 +0100

    choice button added

--- app/src/main/java/com/example/markconroy/rock_paper_scissors/GameActivity.java
+++ app/src/main/java/com/example/markconroy/rock_paper_scissors/GameActivity.java
@@ -2,6 +2,8 @@ package com.example.markconroy.rock_paper_scissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +20,33 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         game = new Game();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        computerChoiceText = (TextView)findViewById(R.id.android_choice_text);
+        userChoiceText = (TextView)findViewById(R.id.user_choice_text);
+        resultText = (TextView)findViewById(R.id.result_text);
+        buttonRock = (Button)findViewById(R.id.btnRock);
+        buttonPaper = (Button)findViewById(R.id.btnPaper);
+        buttonScissors = (Button)findViewById(R.id.btnScissors);
+    }
+
+    public void onMakeChoiceButtonClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnRock:
+                Log.d("RockPaperScissors", "Rock selected");
+                play(Selection.ROCK);
+                break;
+            case R.id.btnPaper:
+                Log.d("RockPaperScissors", "Paper selected");
+                play(Selection.PAPER);
+                break;
+            case R.id.btnScissors:
+                Log.d("RockPaperScissors", "Scissors selected");
+                play(Selection.SCISSORS);
+                break;
+        }
     }
 
 }
